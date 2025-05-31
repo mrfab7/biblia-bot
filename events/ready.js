ebac3e77c636ad3f287e063d23fbf0579ba9644c
mrfab7 <mrfab7@proton.me>
2025-05-31 18:34:59 +0100

    Add event handlers for interaction creation and client readiness

---
+++ events/ready.js
@@ -0,0 +1,10 @@
+const { Events, ActivityType } = require('discord.js');
+
+module.exports = {
+	name: Events.ClientReady,
+	once: true,
+	execute(client) {
+		console.log(`Ready! Logged in as ${client.user.tag}`);
+		client.user.setActivity('John 1:1', { type: ActivityType.Listening });
+	},
+};
