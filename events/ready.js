88af2e14220155c6106b43551e62e5dafedf9383
mrfab7 <mrfab7@proton.me>
2025-06-21 18:19:41 +0100

    Remove activity setting on client ready event

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,9 @@
-const { Events, ActivityType } = require('discord.js');
+const { Events } = require('discord.js');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-		client.user.setActivity('John 1:1', { type: ActivityType.Listening });
 	},
 };
