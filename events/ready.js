caad61b96f9cadd9f0f1542ef28e0f181d5ce592
mrfab7 <mrfab7@proton.me>
2025-07-23 12:43:33 +0100

    database setup

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,12 @@
 const { Events } = require('discord.js');
+const sequelize = require('../db');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
-	execute(client) {
+	async execute(client) {
+		await sequelize.authenticate();
+  		await sequelize.sync();
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
