f180011a78f1439c13cf45a197b4b4bbc6fe8dc8
mrfab7 <mrfab7@proton.me>
2025-07-31 17:43:23 +0100

    Help command documentation

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,8 +1,12 @@
 const { SlashCommandBuilder, EmbedBuilder, MessageFlags } = require('discord.js');
-const emojis = require('../../config.json').emojis || {};
+const emojis = require('../../data/emoji.json') || {};
+const chalk = require('chalk');
 
 module.exports = {
+	name: 'Ping',
 	category: 'utility',
+	description: 'Retrieves the bot\'s latency in milliseconds.',
+	usage: '/ping',
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Retrieves the bot\'s latency.'),
@@ -10,10 +14,10 @@ module.exports = {
 		try {
 			// Get API latency and log it
 			const apiLatency = interaction.client.ws.ping !== undefined ? Math.round(interaction.client.ws.ping) : -1;
-			let pingSymbol = emojis.defaultPing || '🏓';
+			let pingSymbol = '🏓';
 
 			if (apiLatency === -1 || apiLatency === undefined) {
-				console.error('Failed to retrieve API latency.');
+				console.error(chalk.red.bold('Failed to retrieve API latency.'));
 				await interaction.reply({ content: 'Failed to retrieve API latency, please try again later.', flags: MessageFlags.Ephemeral });
 				return;
 			}
@@ -28,15 +32,15 @@ module.exports = {
 				pingSymbol = emojis.highPing || pingSymbol;
 			}
 
-			console.log(`API Latency: ${apiLatency}ms`);
+			console.log(chalk.blue(`API Latency: ${apiLatency}ms`));
 			// Create and send the embed message
 			const embed = new EmbedBuilder()
 				.setColor('#0099ff')
-				.setTitle(`${pingSymbol} Pong! Latency: \`${apiLatency}ms\``)
+				.setTitle(`${pingSymbol} Pong! Latency: \`${apiLatency}ms\``);
 			await interaction.reply({ embeds: [embed] });
 		}
 		catch (error) {
-			console.error('Error executing ping command:', error);
+			console.error(chalk.red.bold('Error executing ping command:'), error);
 			await interaction.reply({ content: 'An error occurred while executing the command. Please try again later.', flags: MessageFlags.Ephemeral });
 		}
 	},
