559157fb44657fc22f9b8c9fe4ab50cfad88bab2
mrfab7 <mrfab7@proton.me>
2025-07-31 17:43:41 +0100

    guild + global command support

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,7 +3,9 @@ const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 
-const commands = [];
+const guildCommands = [];
+const globalCommands = [];
+
 // Grab all the command folders from the commands directory you created earlier
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
@@ -17,7 +19,12 @@ for (const folder of commandFolders) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
 		if ('data' in command && 'execute' in command) {
-			commands.push(command.data.toJSON());
+			if (command.guildOnly) {
+				guildCommands.push(command.data.toJSON());
+			}
+			else {
+				globalCommands.push(command.data.toJSON());
+			}
 		}
 		else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
@@ -29,19 +36,25 @@ const rest = new REST().setToken(token);
 
 (async () => {
 	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+		if (guildCommands.length > 0) {
+			console.log(`Started refreshing ${guildCommands.length} guild (/) commands.`);
+			const guildData = await rest.put(
+				Routes.applicationGuildCommands(clientId, guildId),
+				{ body: guildCommands },
+			);
+			console.log(`Successfully reloaded ${guildData.length} guild (/) commands.`);
+		}
 
-		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
-			// Routes.applicationGuildCommands(clientId, guildId),
-			Routes.applicationCommands(clientId),
-			{ body: commands },
-		);
-
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		if (globalCommands.length > 0) {
+			console.log(`Started refreshing ${globalCommands.length} global (/) commands.`);
+			const globalData = await rest.put(
+				Routes.applicationCommands(clientId),
+				{ body: globalCommands },
+			);
+			console.log(`Successfully reloaded ${globalData.length} global (/) commands.`);
+		}
 	}
 	catch (error) {
-		// And of course, make sure you catch and log any errors!
 		console.error(error);
 	}
 })();
